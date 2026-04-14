17091157c6823b31ef01369d51e3c222a5628506
tojibayevvv <abdullohspersonaldevelopment.28@gmail.com>
2026-04-14 17:50:07 +0500

    gave some styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <SideBar />
+      <AddModal />
     </div>
   );
 }
 
-export default App;
+function SideBar() {
+  return (
+    <div className="sidebar">
+      <p>Sidebar</p>
+    </div>
+  );
+}
+
+function AddModal() {
+  return (
+    <div className="add-modal">
+      <p>Add Modal</p>
+    </div>
+  );
+}
